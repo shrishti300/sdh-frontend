536a6ec168df8a7d6d21cdfff61cb28ee312fd45
shrishti300 <88239226+shrishti300@users.noreply.github.com>
2022-09-27 21:34:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, Suspense, lazy } from "react";
+import logo from './logo.svg';
+import './App.css';
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  Navigate,
+} from "react-router-dom";
+
+import DashBoard from "./pages/DashBoard.js";
+const App=()=> {
+  return (
+    <Router>
+    
+    </Router>
+  );
+}
+
+export default App;
